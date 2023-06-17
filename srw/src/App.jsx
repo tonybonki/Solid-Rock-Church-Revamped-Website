0ef591d2f1da6ee12e805e6f0679ab3cfb3c9983
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-06-17 10:00:53 +0100

    imported hero component

---
+++ srw/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './Components/Hero'
+import HeroWithBackgroundImage from './Components/Hero';
+
+function App() {
+  return (
+    <>
+      <HeroWithBackgroundImage />
+    </>
+  );
+}
+
+export default App;
